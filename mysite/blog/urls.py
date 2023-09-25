941d0cb7f7ba913ee78fae48a7a6185bf1f83de4
Mahdi-Salimi <mahdisalimi.dev@gmail.com>
2023-09-25 17:51:46 +0330

    Create Template Tags
    - use template tags
    - filter by category and author
    - pagination

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,5 +8,7 @@ app_name= 'blog'
 urlpatterns = [
     path('', views.blog_home, name='blog-home'),
     path('<int:pid>/', views.blog_single, name='blog-single'),
-
+    path('category/<str:cat_name>/', views.blog_home, name='blog-category'),
+    path('author/<str:author_username>/', views.blog_home, name='author'),
+    path('search/', views.blog_search, name='blog-search'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 
-def blog_home(request):
+def blog_home(request,cat_name=None, author_username=None):
     posts = Post.objects.all()
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+    if author_username :
+        posts = posts.filter(author__username=author_username)
+        
+    posts = Paginator(posts, 2)
+    try:
+        page_number =request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -12,3 +27,21 @@ def blog_single(request,pid):
     post = get_object_or_404(Post, pk=pid , status=1)
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context) 
+
+def blog_search(request):
+    posts = Post.objects.all()
+    if request.method == 'GET':
+        if search := request.GET.get('s'):
+            posts = posts.filter(content__contains=search)
+        # print(request.GET.get('s'))
+
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
+    
+
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(status=1)
+#     posts = posts.filter(category__name=cat_name)
+#     context = {'posts': posts}
+#     return render(request, 'blog/blog-home.html', context)
